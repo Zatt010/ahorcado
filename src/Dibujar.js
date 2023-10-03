3c600373866f5869ede2ccea3df99b5b03502489
Zatt010 <afuheho1@gmail.com>
2023-10-03 16:51:07 -0400

    Cambio dibujo stickman

--- src/Dibujar.js
+++ src/Dibujar.js
@@ -6,18 +6,31 @@ export const inicializarCanvas = () => {
 };
 
 export const partesCuerpo = [ // Definir las partes del cuerpo del ahorcado como coordenadas [x, y, ancho, alto]
-    [4, 2, 1, 1],
-    [4, 3, 1, 2],
-    [3, 5, 1, 1],
-    [5, 5, 1, 1],
-    [3, 3, 1, 1],
-    [5, 3, 1, 1]
+    [4, 2, 1, 1],     // Cabeza (modificado para que sea redonda)
+    [4.25, 3, 0.5, 2], // Torso
+    [3.3, 4.7, 1, 0.3],    // Pie izquierdo
+    [4.7, 4.7, 1, 0.3],    // Pie derecho
+    [3.3, 3, 1, 0.3],  // Mano izquierda
+    [4.7, 3, 1, 0.3]   // Mano derecha
 ];
 
 export const dibujarParteCuerpo = (ctx, parteCuerpo) => { // Función para dibujar una parte del cuerpo
     ctx.fillStyle = '#000'; // Establecer el color de relleno en negro
-    ctx.fillRect(...parteCuerpo); // Dibujar la parte del cuerpo en el contexto
+
+    if (parteCuerpo === partesCuerpo[0]) { // Si es la cabeza
+        const [x, y, ancho, alto] = parteCuerpo;
+        const radio = ancho / 2; // Calcular el radio para hacerlo redondo
+        const centroX = x + radio;
+        const centroY = y + radio;
+        ctx.beginPath();
+        ctx.arc(centroX, centroY, radio, 0, Math.PI * 2);
+        ctx.fill();
+    } else {
+        ctx.fillRect(...parteCuerpo); // Dibujar las otras partes del cuerpo de forma rectangular
+    }
 };
+
+
 
 export const dibujarAhorcado = (ctx) => { // Función para dibujar el ahorcado
     ctx.canvas.width = 120; // Establecer el ancho del canvas
